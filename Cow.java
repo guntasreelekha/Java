d3be3899234c53ef434a0a90be80bebe0d7e2498
guntasreelekha <guntasreelekha@gmail.com>
2020-08-08 19:05:20 +0530

    features of cow

---
+++ Cow.java
@@ -0,0 +1,16 @@
+public class Cow {
+	int legs = 4;
+	int horns = 2;
+	int tail = 1;
+	String voice ="Moo Moo";
+	public static void main (String args[])
+	{
+		Cow cow=new Cow();
+		System.out.println("no.of legs:"+cow.legs);
+		System.out.println("no.of horns:"+cow.horns);
+		System.out.println("no.of tail:"+cow.tail);
+		System.out.println("cow speaks:"+cow.voice);
+		
+	}
+
+}
