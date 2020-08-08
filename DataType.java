0dce9a4cdb356b0c93b91c3005ca339ec72dc0ba
guntasreelekha <guntasreelekha@gmail.com>
2020-08-08 18:13:41 +0530

    Data types

---
+++ DataType.java
@@ -0,0 +1,17 @@
+public class DataType
+{
+	  public static void main(String[] args)
+	  {
+		    int Num = 5;               
+		    float FloatNum = 5.99f;   
+		    char Letter = 'D';        
+		    boolean Bool = true;       
+		    String Text = "Hello";       
+		    System.out.println(Num);
+		    System.out.println(FloatNum);
+		    System.out.println(Letter);
+		    System.out.println(Bool);
+		    System.out.println(Text);
+	  }
+
+}
