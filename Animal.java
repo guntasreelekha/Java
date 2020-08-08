90b44db1ac5222073c911920a0e221160c957b16
guntasreelekha <guntasreelekha@gmail.com>
2020-08-08 19:33:20 +0530

    animal characteristics

---
+++ Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+    	boolean Vegetarian=false;
+    	String drinks="milk";
+    	int legs=4;
+    	String color ="white";
+    	public static void main(String args[])
+    	{
+    	  Animal cat = new Animal();
+    	  System.out.println("cat is vegetarian:"+cat.Vegetarian);
+    	  System.out.println("cat drinks"+cat.drinks);
+    	  System.out.println("no.of legs to cat:"+cat.legs);
+    	  System.out.println("color of the cat :"+cat.color);
+    }
+}
