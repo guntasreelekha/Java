412277466899c6151fd97f636a4d9ea8776a24f4
guntasreelekha <guntasreelekha@gmail.com>
2020-08-09 20:27:50 +0530

    if statement

---
+++ IfLoop.java
@@ -0,0 +1,18 @@
+public class IfLoop
+{
+	public static void main(String args[])
+	{
+		int legs = 2;
+		if(legs==4)
+		{
+			System.out.println("This is an animal");
+			
+		}
+		else
+		{
+			System.out.println("This is the bird");
+		}
+		
+	}
+
+}
