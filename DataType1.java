27a0629d423a1bee2a10a5c620176a8f3533590f
guntasreelekha <guntasreelekha@gmail.com>
2020-08-09 20:22:21 +0530

    Datatypes

---
+++ DataType1.java
@@ -0,0 +1,25 @@
+public class DataType1 {
+  public static void main(String args[])
+  {
+	  int a =10;
+	  float b = 4.6574789f;
+	  double c = 4.6574789;
+	  char d = 'X';
+	  boolean e = true;
+	  System.out.println("the value of a:"+a);
+	  a++;
+	  System.out.println("now a value:"+a);
+	  System.out.println("the value of b:"+b);
+	  b++;
+	  System.out.println("now value of b:"+b);
+	  System.out.println("the value of c:"+c);
+	  c++;
+	  System.out.println("now  value of c:"+c);
+      System.out.println("d value is ="+d); 
+	  d++;
+	  System.out.println("now d value="+d);
+	  System.out.println(" e value :"+e);
+	  
+	  
+  }
+}
